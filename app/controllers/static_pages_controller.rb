9c91a9e7f62cefe372e58975aca1bb4c132ed9a7
XXX Ahana <ahana@XXXs-Air.hsd1.ca.comcast.net>
2021-01-04 17:53:21 -0800

    Re-factor Controller

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,13 +1,25 @@
-require "pry"
+require 'pry'
 class StaticPagesController < ApplicationController
-
   def home
-    if params[:flickr_user_id]
-      @results = Flickr.photos.search(user_id: params[:flickr_user_id], page: params[:page], per_page: 48)
-    elsif params[:tag]
-      @results = Flickr.photos.search(tags: params[:tag], page: params[:page], per_page: 48, min_upload_date: '2021-01-01')
-    else
-      @results = Flickr.photos.get_interesting(page: params[:page], per_page: 48)
-    end
+    @results = if params[:flickr_user_id]
+                 fetch_by_user_id
+               elsif params[:tag]
+                 fetch_by_tag
+               else
+                 fetch_interesting
+               end
+  end
+
+  def fetch_by_user_id
+    @results = Flickr.photos.search(user_id: params[:flickr_user_id], page: params[:page], per_page: 48)
+  end
+
+  def fetch_by_tag
+    @results = Flickr.photos.search(tags: params[:tag], page: params[:page], per_page: 48,
+                                    min_upload_date: '2021-01-01')
+  end
+
+  def fetch_interesting
+    @results = Flickr.photos.get_interesting(page: params[:page], per_page: 48)
   end
 end
